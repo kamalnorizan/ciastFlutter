a106c8e2af8471fda39f822f6bd6096b344b78b7
Ahmad Kamal B Norizan <kamalnorizan@gmail.com>
2023-08-09 14:20:38 +0800

    User Model

---
+++ lib/Model/user.dart
@@ -0,0 +1,10 @@
+class User {
+  String? nama;
+  String? ic;
+  String? email;
+  String? phone;
+  String? address;
+  int? umur;
+
+  User({this.nama, this.ic, this.email, this.phone, this.address, this.umur});
+}
